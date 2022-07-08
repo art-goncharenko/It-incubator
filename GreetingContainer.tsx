71c97f2d8014a1c4a5687c566c3c98fdda8fb23c
art-goncharenko <98996194+art-goncharenko@users.noreply.github.com>
2022-07-08 00:04:44 -0400

    Add files via upload

---
+++ GreetingContainer.tsx
@@ -0,0 +1,48 @@
+import React, {ChangeEvent, useState} from 'react'
+import Greeting from './Greeting'
+import {UserType} from "./HW3";
+
+type GreetingContainerPropsType = {
+    users: UserType // need to fix any
+    addUserCallback: (users: any) => void // need to fix any
+}
+
+// более простой и понятный для новичков
+// function GreetingContainer(props: GreetingPropsType) {
+
+// более современный и удобный для про :)
+// уровень локальной логики
+const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users, addUserCallback}) => { // деструктуризация пропсов
+    const [name, setName] = useState<any>('') // need to fix any
+    const [error, setError] = useState<any>('') // need to fix any
+
+    const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => { // need to fix any
+        setName(e.currentTarget.value) // need to fix
+    }
+    const addUser = () => {
+        if (name !== ('')) {
+            addUserCallback(name)
+            alert(`Hello  ${name}!`) // need to fix
+            setName('')
+            setError(0)
+        } else {
+            setError('Name is required')
+            setName('')
+        }
+    }
+    console.log(name)
+
+    const totalUsers = 0 // need to fix
+
+    return (
+        <Greeting
+            name={name}
+            setNameCallback={setNameCallback}
+            addUser={addUser}
+            error={error}
+            totalUsers={totalUsers}
+        />
+    )
+}
+
+export default GreetingContainer
